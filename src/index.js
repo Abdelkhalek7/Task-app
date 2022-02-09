2d4fdc49508f18a86921918f55e7b38e241f21a0
Abdelkhalek Ashraf <you@example.com>
2022-02-09 21:13:36 +0200

    intit4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 const express=require('express')
 require('./db/mongoose')
-const request=require('request')
+
 const path=require('path')
 const userRouter =require('./routers/user')
 const taskRouter =require('./routers/task')
